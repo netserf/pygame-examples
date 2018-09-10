50f2dde282d6fe52c0dd863eed6ff638df57ab4b
netserf <networkserf@gmail.com>
2018-09-09 22:56:11 -0700

    Allow car to crash at boundaries

--- a-bit-racey.py
+++ a-bit-racey.py
@@ -9,6 +9,7 @@ version history
 v.0.1.0 - Initial game skeleton that displays the events that appear in event loop of the game
 v.0.2.0 - Adding a car to the display.
 v.0.3.0 - Allow car to move left or right on the screen.
+v.0.4.0 - Create game boundaries.
 """
 
 # display configuration
@@ -22,8 +23,7 @@ red = (255, 0, 0)
 
 # car variables
 car_image = pygame.image.load('assets/car.jpg')
-x_change = 0
-car_speed = 0
+car_width = 210
 
 # (0, 0) is top-left corner
 def car(x, y):
@@ -33,30 +33,35 @@ pygame.init()
 game_display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A Bit Racey')
 clock = pygame.time.Clock()
-x = display_width * 0.45
-y = display_height * 0.5
 
-crashed = False
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        # key movements
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
 
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.6)
+
+    x_change = 0
+    game_exit = False
+
+    while not game_exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_exit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -20
+                if event.key == pygame.K_RIGHT:
+                    x_change = 20
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
         x += x_change
-        # print(event)
-    game_display.fill(white)
-    car(x, y)
-    pygame.display.update()  # updates the entire game surface unless specific arguments provided
-    clock.tick(60)  # frames per second
+        game_display.fill(white)
+        car(x, y)
+        if x > display_width - car_width or x < 0:
+            game_exit = True
+        pygame.display.update()
+        clock.tick(60)
 
+game_loop()
 pygame.quit()
 quit()
